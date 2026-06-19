c66b0fa1e24e08a8764ba122a8fa346435e394ef
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-19 13:40:53 +0500

    document database connection failure error handling

--- config/db.js
+++ config/db.js
@@ -1,21 +1,6 @@
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI);
-//         console.log('MongoDB Connected Successfully!');
-//     } catch (error) {
-//         console.error('MongoDB Connection Failed:', error.message);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// Initialize asynchronous connection to MongoDB database using Mongoose client
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
