fe6260b3e442db2e1451350c5d483bb41a3045c0
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-20 17:14:33 +0500

    fix bug relaod data for teacher dashboard

--- routes/reportsRoutes.js
+++ routes/reportsRoutes.js
@@ -1,10 +1,32 @@
+// const express = require('express');
+// const router = express.Router();
+// const reportsController = require('../controllers/reportsController');
+
+// const { protect, authorize } = require('../middleware/authMiddleware');
+
+// router.get('/dashboard-stats', protect, authorize("super_admin", "admin"), reportsController.getDashboardStats);
+// router.get('/fee-report', protect, authorize("super_admin", "admin"), reportsController.getUnifiedFeeReport);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const reportsController = require('../controllers/reportsController');
 
 const { protect, authorize } = require('../middleware/authMiddleware');
 
-router.get('/dashboard-stats', protect, authorize("super_admin", "admin"), reportsController.getDashboardStats);
-router.get('/fee-report', protect, authorize("super_admin", "admin"), reportsController.getUnifiedFeeReport);
+router.get(
+  '/dashboard-stats', 
+  protect, 
+  authorize("super_admin", "admin", "teacher", "student", "parent", "accountant"), 
+  reportsController.getDashboardStats
+);
+
+router.get(
+  '/fee-report', 
+  protect, 
+  authorize("super_admin", "admin"), 
+  reportsController.getUnifiedFeeReport
+);
 
 module.exports = router;
