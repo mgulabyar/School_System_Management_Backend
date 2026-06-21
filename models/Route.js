63d5af7ac60d6918e6a73e800c3e50417874e5c2
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-21 12:17:30 +0500

    fix: resolve student deactivation status validation enum mismatch

--- models/Route.js
+++ models/Route.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// i make this schema for route mean ways for expert system
+// i make this schema for route mean ways fo
 const routeSchema = new mongoose.Schema({
     routeName: {
         type: String,
