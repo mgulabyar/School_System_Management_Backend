877d18c897b14d3cac2a563f494f27a450dc27c0
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-21 12:40:51 +0500

    implement unified financial reports ledger

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on localhost//port:${PORT}`);
 });
